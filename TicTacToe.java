6c4621cafe7117bb6be6aeacce503c18a2cb8a23
StardustGogeta <liam.gerst@gmail.com>
2018-01-29 13:14:01 -0500

    Main class

---
+++ TicTacToe.java
@@ -0,0 +1,17 @@
+
+/**
+ * Tic-Tac-Toe
+ *
+ * @author Timmy Beuchel, Chase Fenton, Liam Gerst
+ * @version 1-29-18
+ */
+public class TicTacToe {
+    private int wins = 0, losses = 0, ties = 0;
+    
+    public static void main (String[] args) {
+        System.out.println("MEGA TIC-TAC-TOE\n--------------------------\n"+
+        "You must win the Grandmaster board, a normal tic-tac-toe board,\n"+
+        "but you can only move after winning a mini-tic-tac-toe game!");
+        
+    }
+}
