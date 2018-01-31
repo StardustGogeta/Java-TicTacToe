ebabf3e12dbb292375a166c3056a154f634c69c4
cat40 <tbeuch6799@inst.hcpss.org>
2018-01-31 12:44:19 -0500

    fixed checkVictory

--- Board.java
+++ Board.java
@@ -14,38 +14,46 @@ public class Board
         put(2, "O");
         put(3, "O");
     }};
-    
-    public void makeMove(Player player) {
+    public void makeMove(Player player)
+    {
         int[] move = player.getMove();
         int x = move[0], y = move[1];
         if (checkValid(x, y))
             board[x][y] = player.id;
     }
-    
-    public int checkVictory() {
+    public int checkVictory()
+    {
         //check the rows
         for(int[] row : board)
             if(row[0] == row[1] && row[1] == row[2])
                 return row[0];
-        //check the columns
+        //check the collumns
         for(int i = 0; i < board[0].length; i++)
+        {
             if(board[0][i] == board[1][i] && board[1][i] == board[2][i])
                 return board[0][i];
-        //check both diagonals
+        }
+        //check both diagnols
         if(board[0][0] == board[1][1] && board[1][1] == board[2][2] ||
            board[0][2] == board[1][1] && board[1][1] == board[2][0])
            return board[1][1];
+        //check if moves can be made
+        for(int[] row : board)
+            for(int space : row)
+                if(space == 0)
+                    return 0;
         //tie
         return 4;
     }
-    
-    private boolean checkValid(int x, int y) {
+    private boolean checkValid(int x, int y)
+    {
         return board[x][y] == 0;
     }
-    
-    public String toString() {
+    public String toString()
+    {
         String str = "-------\n";
-        for(int[] row : board) {
+        for(int[] row : board)
+        {
             str += "|" + symbols.get(row[0]) + "|" + symbols.get(row[1]) + "|" + symbols.get(row[2]) + "|\n";
             str += "-------\n";
         }
