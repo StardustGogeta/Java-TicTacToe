4b7e29ca7f1257464e5a90c5818aae9c437810c9
wfento5857 <wcfenton12@gmail.com>
2018-01-31 13:18:13 -0500

    started easy bot

---
+++ easyBot.java
@@ -0,0 +1,18 @@
+
+/**
+ * Write a description of class easyBot here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+import java.util.Random;
+public class easyBot extends Player
+{
+    int[] getMove(Board board){
+        Random rand = new Random();
+        
+    }
+    private boolean checkBoard(Board boardy){
+        int[][] board = boardy.getBoard();
+    }
+}
