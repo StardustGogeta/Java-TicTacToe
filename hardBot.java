a2f8ac04834754e85b5fed8acec8036eaf65fd86
cat40 <tbeuch6799@inst.hcpss.org>
2018-01-31 13:16:56 -0500

    started hardBot

---
+++ hardBot.java
@@ -0,0 +1,19 @@
+
+/**
+ * Write a description of class hardBot here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class hardBot extends Player
+{
+    public int id = 3;
+    public int[] getMove(Board board)
+    {
+        //check if there are any 2 in a row on the board
+    }
+    private boolean checkBoard(Board boardy)
+    {
+        int[][] board = boardy.getBoard();
+    }
+}
