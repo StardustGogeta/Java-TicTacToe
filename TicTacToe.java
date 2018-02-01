414b5c0a675a287fbdca8225a65130a0fc38f1f5
StardustGogeta <liam.gerst@gmail.com>
2018-02-01 13:05:41 -0500

    Fixing things

--- TicTacToe.java
+++ TicTacToe.java
@@ -8,8 +8,8 @@ import java.util.Scanner;
 public class TicTacToe {
     private static int sWins = 0, sLosses = 0, sTies = 0, wins = 0, losses = 0, ties = 0;
 
-    private final static int PLAYER1_ID = 1, PLAYER2_ID = 2, TIE_ID = 4;
-    private static Player player1 = new Player(PLAYER1_ID), player2 = new Player(PLAYER2_ID);
+    private final static int PLAYER1_ID = 1, PLAYER2_ID = 2, TIE_ID = -1;
+    private static Player player1 = new easyBot(PLAYER1_ID), player2 = new easyBot(PLAYER2_ID);
     // 1 creates a human, 2 creates a random CPU, 3 creates a difficult CPU
 
     private static int runGame() {
@@ -17,15 +17,19 @@ public class TicTacToe {
         Board miniBoard = new Board();
         int victory = 0;
         int move[];
+        System.out.println("start of game");
         while (true) {
             // Let one player make a move
             miniBoard.makeMove(player1);
             victory = miniBoard.checkVictory();
-            if (victory > 0) return victory;
+            if (victory != 0) return victory;
             // Let other player make a move
             miniBoard.makeMove(player2);
             victory = miniBoard.checkVictory();
-            if (victory > 0) return victory;
+            if (victory != 0) {
+                System.out.println("end of game");
+                return victory;
+            }
         }
     }
 
@@ -36,11 +40,21 @@ public class TicTacToe {
         while (superBoard.checkVictory() == 0) {
             // Run mini-game
             int winner = runGame();
+            System.out.println("YAY!");
             if (winner != TIE_ID) {
-                if (winner == PLAYER1_ID) superBoard.makeMove(player1);
-                else superBoard.makeMove(player2);
+                if (winner == PLAYER1_ID) {
+                    wins++;
+                    superBoard.makeMove(player1);
+                }
+                else {
+                    losses++;
+                    superBoard.makeMove(player2);
+                }
             }
-            else System.out.println("It's a tie! Time to play again!");
+            else {
+                ties++;
+                System.out.println("It's a tie! Time to play again!");
+            }
         }
         return superBoard.checkVictory();
     }
@@ -49,7 +63,7 @@ public class TicTacToe {
         Scanner scan = new Scanner(System.in);
         System.out.println("MEGA TIC-TAC-TOE\n--------------------------\n"+
             "You must win the \"Grandmaster board\", a normal tic-tac-toe board,\n"+
-            "but you can only move after winning a mini-tic-tac-toe game!");
+            "but you can only move after winning a mini-tic-tac-toe game! "+player1.getID());
         while (true) {
             int superWinner = runSuperGame();
             
