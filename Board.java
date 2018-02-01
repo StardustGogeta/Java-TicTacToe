997af67b31c8e89dab31f4b6f04c5bcd5883645f
cat40 <tbeuch6799@inst.hcpss.org>
2018-02-01 12:53:22 -0500

    fixed checkVictory()

--- Board.java
+++ Board.java
@@ -35,16 +35,16 @@ public class Board
     {
         //check the collums
         for(int[] row : board)
-            if(row[0] == row[1] && row[1] == row[2])
+            if(row[0] == row[1] && row[1] == row[2] && row[1] != 0)
                 return row[0];
         //check the rowws
         for(int i = 0; i < board[0].length; i++)
         {
-            if(board[0][i] == board[1][i] && board[1][i] == board[2][i])
+            if(board[0][i] == board[1][i] && board[1][i] == board[2][i] && board[1][i] != 0)
                 return board[0][i];
         }
         //check both diagnols
-        if(board[0][0] == board[1][1] && board[1][1] == board[2][2] ||
+        if(board[1][1] != 0 && (board[0][0] == board[1][1] && board[1][1] == board[2][2] ||
         board[0][2] == board[1][1] && board[1][1] == board[2][0])
             return board[1][1];
         //check if moves can be made
