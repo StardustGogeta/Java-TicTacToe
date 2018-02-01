39e7407b6c80a660a624f8ff127694e0da132b7a
wfento5857 <wcfenton12@gmail.com>
2018-02-01 12:52:17 -0500

    added playerInput class

---
+++ playerInput.java
@@ -0,0 +1,34 @@
+
+/**
+ * Write a description of class playerInput here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+import java.util.Scanner;
+public class playerInput extends Player
+{
+    public int id = 1;
+    public playerInput(){}
+    public playerInput(int id){
+        this.id = id;
+    }
+    int[] getMove(Board board){
+        Scanner input = new Scanner(System.in);
+        int x,y;
+        boolean check = false;
+        do{
+            System.out.println("Input your move's \'X\' coordinate");
+            x = input.nextInt();
+            input.nextLine();
+            System.out.println("Input your move's \'Y\' coordinate");
+            y = input.nextInt();
+            input.nextLine();
+            if (board.checkValid(x,y))
+                check = true;
+        }
+        while (!check);
+        int[] a = {x,y};
+        return a;
+    }   
+}
