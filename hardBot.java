ceff3a581e1abc93127778615503aa6d9477515b
cat40 <19844790+cat40@users.noreply.github.com>
2018-02-01 19:13:24 -0500

    Finished hardBot

--- hardBot.java
+++ hardBot.java
@@ -35,36 +35,46 @@ public class hardBot extends Player
     /**
      * Checks the board for any two in a line
      * Returns the space a move must be made in, if it exists (x, y)
-     * currently only detects 2 in a row, not when the middle can be completed (fix later)
      * carefully test for switching of x and y
-     * add checking for 0. 
+
      */
     private int[] checkBoard(int[][] board)
     {
         // columns
         for(int i=0; i<board.length; i++)
-            if(board[i][0] == board[i][1] && board[i][2] == 0)
+            if(board[i][0] == board[i][1] && board[i][2] == 0 && board[i][0] != 0)
                 return new int[]{2, i};
-            else if(board[i][1] == board[i][2] && board[i][0] == 0)
+            else if(board[i][1] == board[i][2] && board[i][0] == 0 && board[i][1] != 0)
                 return new int[]{0, i};
+            //check space in between
+            else if(board[i][0] == board[i][2] && board[i][0] != 0 && board[i][1] == 0)
+                return new int[]{1, i};
         // rows
         for(int i = 0; i < board[0].length; i++)
         {
-            if(board[0][i] == board[1][i] && board[2][i] == 0)
+            if(board[0][i] == board[1][i] && board[2][i] == 0 && board[0][i] != 0)
                 return new int[]{i, 2};
-            else if (board[1][i] == board[2][i] && board[0][i] == 0)
+            else if (board[1][i] == board[2][i] && board[0][i] == 0 && board[1][i] != 0)
                 return new int[]{i, 0};
+            // check space in between
+            else if (board[0][i] == board[2][i] && board[1][i] == 0 && board[0][1] != 0)
+                return new int[]{1, 0};
         }
         // diagonals
-        if(board[0][0] == board[1][1] && board[2][2] == 0)
+        if(board[0][0] == board[1][1] && board[2][2] == 0 && board[0][0] != 0)
             return new int[]{2, 2};
-        else if (board[1][1] == board[2][2] && board[0][0] == 0)
+        else if (board[1][1] == board[2][2] && board[0][0] == 0 && board[1][1] != 0)
             return new int[]{0, 0};
-        else if (board[0][2] == board[1][1] && board[2][0] == 0)
+        else if (board[0][2] == board[1][1] && board[2][0] == 0 && board[0][2] != 0)
             return new int[]{0, 2};
-        else if(board[1][1] == board[2][0] && board[0][2] == 0)
+        else if(board[1][1] == board[2][0] && board[0][2] == 0 && board[1][1] != 0)
             return new int[] {2, 0};
+        //don't need to check space in between because middle space is claimed later if possible. 
         
+        //claim middle space if possible
+        if (board[1][1] == 0)
+            return new int[] {0, 0};
+            
         //nothing applies
         return null;
     }
